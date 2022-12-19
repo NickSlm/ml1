e671226630f82cc0b8b4ed810304f3600f75ed46
NickSlm <solomonovnick@gmail.com>
2022-12-19 13:59:42 +0200

    chapter 3 - classification

--- chp2.py
+++ chp2.py
@@ -1,19 +1,27 @@
-from utils import fetch_housing_data,load_housing_data, split_test_train_by_id, CombinedAttributeAdder
+from utils import fetch_housing_data, load_housing_data, CombinedAttributeAdder, display_score, save_model_to_disk, load_model_from_disk, top_k_indices, TopFeatures
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import StratifiedShuffleSplit, cross_val_score, GridSearchCV,RandomizedSearchCV
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, StandardScaler
+from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
 import pandas as pd
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 from zlib import crc32
 import numpy as np
+from scipy.stats import randint, reciprocal, expon
 
 def main():
-
-    fetch_housing_data()
+    # VARIABELS
+    k = 5
+    # 
+    
+    # fetch_housing_data()
     data_frame = load_housing_data()
     data_frame['income_cat'] = pd.cut(data_frame['median_income'],bins=[0., 1.5, 3.0, 4.5, 6, np.inf], labels=[1,2,3,4,5])
     split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
@@ -23,27 +31,12 @@ def main():
     for set_ in (strat_train_set, strat_test_set):
         set_.drop("income_cat", axis=1, inplace=True)
     
-    # strat_train_set.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
-    #                      s=strat_train_set["population"] / 100, label="population", figsize=(10,7),
-    #                      c="median_house_value",colormap="jet", colorbar=True)
-
-    # FIRST WAY TO CHECK CORRELATION    
-    # corr_matrix = strat_train_set.corr()
-    # print(corr_matrix['median_house_value'].sort_values(ascending=False))
-    
-    # SECOND WAY TO CHECK CORRELATION BETWEEN ALL THE ATTRIBUTES LISTED
-    # attributes = ["median_house_value", "median_income", "total_rooms","housing_median_age"]
-    # scatter_matrix(strat_train_set[attributes], figsize=(12, 8))
-    
     housing = strat_train_set.drop("median_house_value", axis=1)
     housing_labels = strat_train_set["median_house_value"].copy()
     
-    imputer = SimpleImputer(strategy="median")
+
     housing_num = housing.drop("ocean_proximity", axis=1)
-    imputer.fit(housing_num)
-    X = imputer.transform(housing_num)
-    housing_tr = pd.DataFrame(X, columns=housing_num.columns,index=housing_num.index)
-    
+ 
     # Text and Categorical attributes changes (Changing non interger attributes to integer)
     housing_cat = housing[["ocean_proximity"]]
     cat_encoder = OneHotEncoder(sparse=False)
@@ -71,16 +64,35 @@ def main():
             ("num", num_pipeline, num_attribs),
             ("cat", OneHotEncoder(), cat_attribs),
         ])
+ 
     housing_prepared = full_pipeline.fit_transform(housing)
-    lin_reg = LinearRegression()
-    lin_reg.fit(housing_prepared,housing_labels)
     
-    some_data = housing.iloc[:5]
-    some_lables = housing_labels.iloc[:5]
-    some_data_prepared = full_pipeline.transform(some_data)
-    
-    print(lin_reg.predict(some_data_prepared))
-    print(list(some_lables))
+    param_grid = [
+    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+    ]
 
-if __name__ == "__main__":
-    main()
+    forest_reg = RandomForestRegressor()
+    grid_search = GridSearchCV(forest_reg, param_grid, cv=5, scoring='neg_mean_squared_error', verbose=2)
+    grid_search.fit(housing_prepared, housing_labels)
+    
+    feature_importances = grid_search.best_estimator_.feature_importances_
+    
+    svr = SVR()
+    param_distribs = {
+        'kernel': ['rbf'],
+        'C': reciprocal(20, 200000),
+        'gamma': expon(scale=1.0),
+    }  
+    rnd_search = RandomizedSearchCV(svr, param_distributions=param_distribs,
+                                    verbose=2, cv=5, scoring='neg_mean_squared_error', random_state=42)
+    rnd_search.fit(housing_prepared, housing_labels)
+    
+    prepare_select_and_predict_pipeline = Pipeline([('preparation', full_pipeline),
+                                                    ('feature_selection', TopFeatures(feature_importances, k)),
+                                                    ('prediction', SVR(**rnd_search.best_params_))])
+    
+    print(prepare_select_and_predict_pipeline.fit(housing, housing_labels))
+    
+    
+main()
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
 import tarfile
 import urllib
+import joblib
 from urllib import request
 from sklearn.base import BaseEstimator, TransformerMixin
 import pandas as pd
@@ -25,14 +26,29 @@ def load_housing_data(housing_path=HOUSING_PATH):
 
 def check_if_test(id, ratio):
     return crc32(np.int64(id)) < ratio * 2 ** 32
-    
+
 def split_test_train_by_id(data, ratio, index):
     data_ids = data[index]
     test_set = data_ids.apply(lambda id: check_if_test(id, ratio))
     return data.loc[~test_set], data.loc[test_set]
 
+def display_score(score):
+    print("Scores:", score)
+    print("Mean:", np.mean(score))
+    print("Standard deviation:", np.std(score))
     
-    
+def save_model_to_disk(model_name, file_name):
+    """
+    Save model to disk
+    Args:
+        model_name (object): _description_
+        file_name (str): filename
+    """
+    joblib.dump(model_name, file_name)
+
+def load_model_from_disk(file_name):
+    return joblib.load(file_name)
+
 class CombinedAttributeAdder(BaseEstimator, TransformerMixin):
     rooms_ix, households_ix, population_ix, bedrooms_ix = 3, 4, 5, 6
     def __init__(self, add_bedrooms_per_room = True):
@@ -48,5 +64,17 @@ class CombinedAttributeAdder(BaseEstimator, TransformerMixin):
         else:
             return np.c_[X, rooms_per_household, population_per_household]
 
+def top_k_indices(features, k):
+    return np.sort(np.argpartition(np.array(features), -k)[-k:])
 
-
+class TopFeatures(BaseEstimator, TransformerMixin):
+    def __init__(self, array, k):
+        self.array = array
+        self.k = k
+    
+    def fit(self, X,y=None):
+        self.feature_importances_ = top_k_indices(self.array, self.k)
+        return self
+    
+    def transform(self, X):
+        return X[:,self.feature_importances_]
