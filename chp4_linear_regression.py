bed3fce1e2334da225746ebed5c098c00b148a29
NickSlm <solomonovnick@gmail.com>
2023-01-12 16:34:11 +0200

    deleted file

--- chp4_linear_regression.py
+++ chp4_linear_regression.py
@@ -3,19 +3,20 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
+
+# linear equation
 x  = 2 * np.random.rand(100, 1)
 y =  4 + 3 * x + np.random.randn(100, 1)
 
-
-
-plt.plot(x,y,"y.")
-plt.xlabel("$x_1$",fontsize=18)
-plt.ylabel("$y$",rotation=0,fontsize=18)
-plt.axis([0,2,0,15])
-plt.show()
+# plt.plot(x,y,"y.")
+# plt.xlabel("$x_1$",fontsize=18)
+# plt.ylabel("$y$",rotation=0,fontsize=18)
+# plt.axis([0,2,0,15])
+# plt.show()
 
 
 x_b = np.c_[np.ones((100,1)),x]
+print(x_b)
 # normal equation - linear regression => (xT*x)^-1*xT*y
 # theta_best = np.linalg.inv(x_b.T.dot(x_b)).dot(x_b.T).dot(y)
 # print(theta_best)
@@ -29,13 +30,14 @@ x_b = np.c_[np.ones((100,1)),x]
 # theta_best_svd, residuals, rank, s = np.linalg.lstsq(x_b, y, rcond=1e-6)
 # print(theta_best_svd)
 
-# Gradient Descent
-learning_rate = 0.1
-n_iterations = 100
-m = 100
+# Batch Gradient Descent
+# learning_rate = 0.1
+# n_iterations = 1000
+# m = 100
 
-theta = np.random.randn(2,1)
+# theta = np.random.randn(2,1)  
 
-for iteration  in range(n_iterations):
-    gradient = 2/m * x_b.T.dot(x_b.dot(theta) - y)
-    theta = theta - learning_rate * gradient
+# for iteration  in range(n_iterations):
+#     gradient = 2/m * x_b.T.dot(x_b.dot(theta) - y)
+#     theta = theta - learning_rate * gradient
+# print(theta)    
